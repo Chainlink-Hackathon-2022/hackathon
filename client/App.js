dcdaa47bffe401290cd79fb904094ebc6abe029d
Brooke Wilder <brookewilder19@gmail.com>
2022-04-27 14:45:55 -0400

    first commit

---
+++ client/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import Navbar from './components/Navbar'
+import Routers from './Routes'
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <Routers />
+    </div>
+  )
+}
+
+export default App
